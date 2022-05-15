ec1dba6731f34646f675793961f9a2e2381f9485
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-05-15 22:27:01 +0500

    1st Example of Dowhile loop

---
+++ Increasingno.java
@@ -0,0 +1,9 @@
+public class Increasingno {    
+public static void main(String[] args) {    
+    int k=1;    
+    do{    
+        System.out.println(i);    
+    k++;    
+    }while(k<=10);    
+}    
+}    
