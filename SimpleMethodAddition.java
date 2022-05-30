bb71b8361448fc5e8058be3e77411fc86290fe6a
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-05-30 23:33:11 +0500

    Example of Simple "Method" in java

---
+++ SimpleMethodAddition.java
@@ -0,0 +1,21 @@
+class SimpleMethodAddition
+{
+  int x;
+  int y;
+  void add()
+  {
+   
+System.out.println(x+y);
+  }
+}
+public class Lab6
+{
+  public static void main(String args[])
+{
+  Addition P = new Addition();
+  P.x = 10;
+  P.y = 20;
+  P.add();
+}
+
+}
