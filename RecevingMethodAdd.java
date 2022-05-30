1dc8043874a41d888cccea4650649b18543a2be2
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-05-30 23:35:03 +0500

    Example Of Parametrized Method(Method Receving)

---
+++ RecevingMethodAdd.java
@@ -0,0 +1,23 @@
+class RecevingMethodAdd
+{
+  int x;
+  int y;
+  void add(int a,int b)
+  {
+   
+ 	x = a;
+	y = b;
+
+    System.out.println(x+y);
+  }
+}
+public class Lab6
+{
+  public static void main(String args[])
+{
+  Addition P = new Addition();
+ 
+  P.add(40,70);
+}
+
+}
