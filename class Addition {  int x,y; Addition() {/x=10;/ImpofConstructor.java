c7f59c8a12b88ccf265aee828795e8e8eac7b7a0
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-06-08 05:41:43 +0500

    void return example of constructor in java

---
+++ class Addition {  int x,y; Addition() {/x=10;/ImpofConstructor.java
@@ -0,0 +1,19 @@
+class Addition
+{
+ int x,y;
+Addition()
+{
+//  x=10;
+ // y=20;
+}
+}
+public class ImpofConstructor
+{
+  public static void main(String[] args)
+{
+Addition P = new Addition();
+
+
+System.out.println(P.x+P.y);
+}
+}
