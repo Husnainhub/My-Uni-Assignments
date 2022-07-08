c0dc8efb02da1dab37929039665c464fe6eeca61
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-07-08 15:53:39 +0500

    Use of This Keyword in java

---
+++ thiskey.java
@@ -0,0 +1,17 @@
+class Addition
+{
+	int x,y;
+	Addition(int x,int y)
+   {
+	this.x=x;
+	this.y=y;
+	System.out.print("x="+x+"\t"+"y="+y);	
+   }
+}
+public class thiskey
+{
+public static void main (String arg[])
+  {
+    Addition p=new Addition(56,22);
+  }
+}
