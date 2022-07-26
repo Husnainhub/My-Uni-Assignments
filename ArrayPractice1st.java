8f20e722dbf61b1574ff1f1ee54804e968693648
Husnain Abbas <96606630+Husnainhub@users.noreply.github.com>
2022-07-26 17:51:42 +0500

    Add files via upload
    passing 1d array to function

--- ArrayPractice1st.java
+++ ArrayPractice1st.java
@@ -1,17 +1,19 @@
+class Array
+{
+	void display(int a[])
+	{
+		for(int i=0;i<=4;i++)
+			System.out.println(a[i]);
+	}
+	
+}
+
 public class ArrayPractice1st
 {
 	public static void main(String arg[])
 	{
-		int x[] = new int [5];
-		x[0] = 20;
-		x[1] = 30;
-		x[2] = 40;
-		x[3] = 50;
-		x[4] = 60;
-		System.out.println(x[0]);
-		System.out.println(x[1]);
-		System.out.println(x[2]);
-		System.out.println(x[3]);
-		System.out.println(x[4]);
+		int x[] = {20,30,40,50,60};
+		Array obj = new Array();
+		obj.display(x);// x stores 2000
 	}
 }
